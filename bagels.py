e241eee1af24e55aa6003f4709fbcf01ad06d867
Mateusz Lorenc <MALOR@orsted.com>
2022-08-12 08:32:42 +0200

    first commit

---
+++ bagels.py
@@ -0,0 +1,13 @@
+import random
+
+secret_num = random.randrange(100, 999)
+
+print(secret_num)
+
+fermi = 'One digit is correct but in wrong position'
+pico = 'One digit is correct and in right position'
+bagels = 'No digit is correct'
+
+print('''Welcome to the game, you have 8 guesses to guess a 3 digit number
+I'll give you hints on the way''')
+
